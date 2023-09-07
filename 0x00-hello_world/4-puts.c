c1d08ec1a5bc0895e0f738d5a1d788e43263cea4
Arowolokehinde <arowolokehinde321@gmail.com>
2023-09-07 16:09:00 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main- This is the entry point for C programming language
- * Return: End the value with 0
+ * main - This is c programming language
+ * Return: Always 0 (success)
 */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
