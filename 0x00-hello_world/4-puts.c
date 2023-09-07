8d4ec0d685ca1debd83a3b989f4d28c7a0190202
Arowolokehinde <arowolokehinde321@gmail.com>
2023-09-07 15:59:25 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main- This is the entry point for C programming language
+ * Return: End the value with 0
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
+}
