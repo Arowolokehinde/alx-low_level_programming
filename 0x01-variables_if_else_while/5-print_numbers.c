842610db0c7bbd9c253f0bb5f5b172bbe4e86911
Arowolokehinde <arowolokehinde321@gmail.com>
2023-09-09 10:33:12 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point where all the code is
+ * Description: low level programming
+ * Return: Always 0 success
+*/
+
+int main(void)
+{
+	int number;
+
+	for (number = 0; number <= 10; number++)
+	{
+		printf("%d\n", number);
+	}
+
+	return (0);
+}
