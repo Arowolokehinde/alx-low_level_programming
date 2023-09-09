66b7debffbcc4ab20ffb37a6ac5191f3c1e3e50f
Arowolokehinde <arowolokehinde321@gmail.com>
2023-09-09 11:05:21 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point for the code
+ * Description: low level programming
+ * Return: Always 0 successs
+*/
+
+int main(void)
+{
+	int number;
+
+	for (number == 0; number < 10; number++)
+	{
+		putchar(number);
+	}
+	putchar('\n');
+		return (0);
+}
