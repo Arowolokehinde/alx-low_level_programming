7c1d10a8a2c57456cce520f2dea6c66defb4f998
Arowolokehinde <arowolokehinde321@gmail.com>
2023-09-09 10:17:21 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point for all the code
+ * Description: low level programming
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+	if (letter != 'q' && 'e')
+	putchar(letter);
+	}
+	putchar('\n');
+
+	return (0);
+}
