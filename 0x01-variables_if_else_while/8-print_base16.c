ac76f24ec309e3a59a828f0f9042906ed74dbd93
Arowolokehinde <arowolokehinde321@gmail.com>
2023-09-09 13:37:46 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,20 +1,27 @@
 #include <stdio.h>
+
+
 /**
- * main - entry point
- * Description: low level
- * Return: Always 0
-*/
+ * main- prints all possible combinations of single-digit numbers
+ *
+ * Return: Always 0.
+ *
+ */
 
-int main(void)
+	int main(void)
 {
-	int num;
-	char letter;
+		int num;
 
-	for (num = 0; num < 10; num++)
+	for (num = 0; num <= 9; num++)
+	{
 		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
 
-	for (letter = 'a'; letter <= 'f'; letter++)
-		putchar(letter);
+		putchar(',');
+		putchar(' ');
+
+	}
 
 	putchar('\n');
 	return (0);
