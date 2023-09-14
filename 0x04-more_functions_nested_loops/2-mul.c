78eff0ca22b2baeb597abad086c63c8da9a57316
Arowolokehinde <arowolokehinde321@gmail.com>
2023-09-14 14:46:46 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul- multiply int a and int b
+ * @a: int a
+ * @b: int b
+ * Return: multiplication of a and b
+*/
+
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}
