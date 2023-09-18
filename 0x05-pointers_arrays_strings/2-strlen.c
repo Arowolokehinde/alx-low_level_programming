8b49ae01022e6b3bc3bc9f4337802c759680c9d8
Arowolokehinde <arowolokehinde321@gmail.com>
2023-09-18 10:05:52 -0700

    2-strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+/**
+ * _strlen - function that returns the length of a string.
+ * @s: string
+ * Return: always success
+ */
+int _strlen(char *s)
+{
+	int n;
+
+	for (n = 0; *s != '\0'; n++)
+		s++;
+
+	return (n);
+}
