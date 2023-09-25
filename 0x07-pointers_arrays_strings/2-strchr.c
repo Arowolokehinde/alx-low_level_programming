27d2ec05641a9a7f846b3384a6a7dcf6b3da2af9
Arowolokehinde <arowolokehinde321@gmail.com>
2023-09-25 16:24:02 -0700

    2-strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strchr -  a pointer to the first occurrence of the character
+ * @s: input
+ * @c: input
+ * Return: Always 0 (Success)
+*/
+
+
+char *_strchr(char *s, char c)
+{
+	int j = 0;
+
+	for (; s[j] >= '\0'; j++)
+	{
+		if (s[j] == c)
+			return (&s[j]);
+	}
+	return (0);
+}
