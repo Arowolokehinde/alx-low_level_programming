47101c8dcc7d9659cb498c1c5abae14e54885b8f
Arowolokehinde <arowolokehinde321@gmail.com>
2023-10-10 16:27:56 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog- variable name
+ * @name: variable for char
+ * @age: variable for float
+ * @owner: variable for char
+ * return: always success
+*/
 struct dog
 {
 	char *name;
