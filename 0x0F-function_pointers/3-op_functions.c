7c914e577ed3730fde2b5b8dc411470efaff2c20
Arowolokehinde <arowolokehinde321@gmail.com>
2023-10-11 09:01:40 -0700

    print_name

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "3-calc.h"
+
+/**
+ * op_add- fuction that adds
+ * @a: first integer
+ * @b: second integer
+ * Return: always success
+*/
+
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -3,5 +3,6 @@
 
 int int_index(int *array, int size, int (*cmp)(int));
 int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
 
 #endif
