bdebab017ebbc2399127afde2711a06310e3d0f4
Arowolokehinde <arowolokehinde321@gmail.com>
2023-10-30 14:28:04 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * create_file - function that creates a file.
+ * @filename: The file to be modified
+ * @text_content: The text
+ * Return: Always success
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int i, op, wr;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content != NULL)
+	{
+		for (i = 0; text_content[i];)
+			i++;
+	}
+	else
+		i = 0;
+
+	op = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	wr = write(op, text_content, i);
+	if (op == -1)
+		return (-1);
+	close(op);
+	return (1);
+}
