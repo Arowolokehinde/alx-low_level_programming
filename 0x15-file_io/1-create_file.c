7beaed3c926000f8fdc59750764a4eace14b185d
Arowolokehinde <arowolokehinde321@gmail.com>
2023-10-30 14:33:07 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,7 +9,7 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int i, op, wr;
+	int i, op;
 
 	if (filename == NULL)
 		return (-1);
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 		i = 0;
 
 	op = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	wr = write(op, text_content, i);
+	write(op, text_content, i);
 	if (op == -1)
 		return (-1);
 	close(op);
